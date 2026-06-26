a50467c446c67c1a6191291578caacf4dd6ac18e
inininda <mawarnininda@gmail.com>
2026-06-26 14:34:02 +0700

    chore: update navbar data

--- src/data/navData.ts
+++ src/data/navData.ts
@@ -8,6 +8,7 @@ export const navData: NavItem[] = [
   { label: 'Home', href: '#home' },
   { label: 'About', href: '#about' },
   { label: 'Skills', href: '#skills' },
-  { label: 'Works', href: '#works' },
+  { label: 'Projects', href: '#projects' },
+  { label: 'Experience', href: '#experience' },
   { label: 'Contact', href: '#contact' },
 ]
